9dea5f3975e3f9ae807e2c1e56340625d11da4a7
ArturoLinaresArancibia <a.linares@outlook.com>
2025-10-14 18:16:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Pega aquí tus credenciales públicas (se pueden exponer en el cliente)
-export const SUPABASE_URL = "https://TU-PROYECTO.supabase.co";
-export const SUPABASE_ANON_KEY = "TU_ANON_KEY";
+export const SUPABASE_URL = "https://aeuquxvkybwywmvifehj.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFldXF1eHZreWJ3eXdtdmlmZWhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0NTY2MjMsImV4cCI6MjA3NjAzMjYyM30.xmByl82oDJy_jVqwhv5918a3fTqmoumWHbmWHpa8kPE";
