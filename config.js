29acf704d4f2ee4800006678e894d800847730c2
ArturoLinaresArancibia <a.linares@outlook.com>
2025-10-15 10:39:53 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Pega aquí tus credenciales públicas (se pueden exponer en el cliente)
-export const SUPABASE_URL = "https://aeuquxvkybwywmvifehj.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFldXF1eHZreWJ3eXdtdmlmZWhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0NTY2MjMsImV4cCI6MjA3NjAzMjYyM30.xmByl82oDJy_jVqwhv5918a3fTqmoumWHbmWHpa8kPE";
+export const SUPABASE_URL = "https://epcfgyevzyojaxeqfmhx.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwY2ZneWV2enlvamF4ZXFmbWh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MzU1MjQsImV4cCI6MjA3NjExMTUyNH0.QbQebuHbDyjsfbmq5n7Wb9z8blD6d67ni-QyI0Vez_g";
 export const EXTERNAL_PORTAL_URL = "https://arturolinaresarancibia.github.io/Mapa-de-riesgo-DRT";
